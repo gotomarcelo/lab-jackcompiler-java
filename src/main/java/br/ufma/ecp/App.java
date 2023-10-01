5bf0c357961a54a6b1b212aad8cc1ed4ca528ca2
gotomarcelo <gotomarcelo98@gmail.com>
2023-10-01 19:58:12 -0300

    feat: :sparkles: New feature to recognize strings into compiler

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -15,10 +15,10 @@ public class App
     {
 
     
-        String input = "45 variavel + while < , if";
+        String input = "45 \"hello\" variavel + while < , if";
         Scanner scan = new Scanner (input.getBytes());
         for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
-                System.out.println(tk);
+            System.out.println(tk);
         }
 
         /*
