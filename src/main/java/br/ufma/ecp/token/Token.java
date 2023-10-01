050f9c623044a0da957c59cedc0b9df068a1584c
gotomarcelo <gotomarcelo98@gmail.com>
2023-10-01 20:43:17 -0300

    feat: :sparkles: Adding Line counter into compiler

--- src/main/java/br/ufma/ecp/token/Token.java
+++ src/main/java/br/ufma/ecp/token/Token.java
@@ -3,10 +3,12 @@ public class Token {
 
     public final TokenType type;
     public final String lexeme;
-
-    public Token (TokenType type, String lexeme) {
-        this.type = type;
-        this.lexeme = lexeme;
+    final int line;
+  
+    public Token (TokenType type, String lexeme, int line) {
+           this.type = type;
+           this.lexeme = lexeme;
+           this.line = line;
     }
 
     public String toString() {
